1416c3ef2913178882f6294d26142d7bb1959b4b
Jan Jablunka <jablunka@gmail.com>
2016-05-23 10:42:42 +0200

    incremental

--- src/Tesco.php
+++ src/Tesco.php
@@ -4,8 +4,31 @@ namespace Chakula;
 
 class Tesco {
 
-	public function getSuperDepartments() {
+	const BASE_URL = 'https://nakup.itesco.cz/groceries/cs-CZ/';
 
+	public function getSuperDepartments() {
+		$url = static::BASE_URL;
+		$src = \Katu\Utils\Cache::getUrl($url, 86400);
+
+		echo $src; die;
+
+		preg_match('#data-props="(.+)"#U', $src, $match);
+		$res = \Katu\Utils\JSON::decodeAsObjects(html_entity_decode($match[1]));
+
+		$departments = array_filter($res->nav, function($i) {
+			return !in_array($i->name, \Katu\Config::get('tesco', 'departments', 'exclude'));
+		});
+
+		$categoryIds = [];
+		foreach ($departments as $department) {
+			foreach ($department->items as $item) {
+				foreach ($item->items as $category) {
+					$categoryIds[] = $category->catId;
+				}
+			}
+		}
+
+		return $categoryIds;
 	}
 
 }
